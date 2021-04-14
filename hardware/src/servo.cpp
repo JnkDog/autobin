80279e90c724f37eb4022888a9d1d555195a2c0b
yuyangovo <zhouccwinnie@163.com>
2021-04-14 20:20:57 +0800

    servo open

--- hardware/src/servo.cpp
+++ hardware/src/servo.cpp
@@ -1,12 +1,21 @@
 
 #include <iostream>
-//#include <time.h>
 #include <unistd.h> 
-//#include <Adafruit_PCA9685.h>
 #include <stdint.h>
 #include <stdio.h>
 #include "PCA9685.h"
-PCA9685 PWM = PCA9685(); // called this way, it uses the default address 0x40
+
+#define SERVOMIN  150 
+// this is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  600 
+// this is the 'maximum' pulse length count (out of 4096)
+
+PCA9685 PWM = PCA9685(); 
+// called this way, it uses the default address 0x40
+/*
+you can also call it with a different address you want
+PCA9685 pwm = PCA9685(0x41);
+*/
 #include "servo.h"
 
 servo::servo() {
@@ -17,7 +26,7 @@ servo::servo() {
 }
 
 servo::~servo() {
-	//delete i2c;
+	
 }
 
 void servo::set_servo_open (int channel) {
@@ -34,14 +43,14 @@ void servo::set_servo_open (int channel) {
 		}
 	}
     */
-    PCA9685.setPWM(channel, 0, SERVOMAX);
-    printf("%d号舵机开启%d\n",channel,SERVOMAX);
+    PWM.setPWM(channel, 0, SERVOMAX);
+    printf("No.%d servo opens %d\n",channel,SERVOMAX);
 }
 
 
 void servo::set_servo_close (int channel) {
-    //set_servo_open(int channel);
+    
     sleep(2);
     PWM.setPWM(channel, 0, SERVOMIN);
-    printf("%d号舵机开启%d\n",channel,SERVOMIN);
+    printf("No.%d servo closes %d\n",channel,SERVOMIN);
 }
