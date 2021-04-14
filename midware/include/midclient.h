e8d99f5737ef80b0144477c4ff298e5019711cf7
Pengchong <penchoallen@gmail.com>
2021-04-14 17:17:05 +0800

    add socket and resove some bug

--- midware/include/midclient.h
+++ midware/include/midclient.h
@@ -12,32 +12,32 @@ int sendtomyserver(char bufdata[])
 {
 	int client_sockfd;
 	int len;
-	struct sockaddr_in remote_addr; //服务器端网络地址结构体
+	struct sockaddr_in remote_addr; //Server network address structure
 	int sin_size;
-	char buf[BUFSIZ];  //数据传送的缓冲区
-	memset(&remote_addr,0,sizeof(remote_addr)); //数据初始化--清零
-	remote_addr.sin_family=AF_INET; //设置为IP通信
-	remote_addr.sin_addr.s_addr=inet_addr("127.0.0.1");//服务器IP地址
-	remote_addr.sin_port=htons(8000); //服务器端口号
+	char buf[BUFSIZ];  //The buffer for data transfer
+	memset(&remote_addr,0,sizeof(remote_addr)); //Data initialization-set zero
+	remote_addr.sin_family=AF_INET; //Set to IP communication
+	remote_addr.sin_addr.s_addr=inet_addr("127.0.0.1");//Server IP address
+	remote_addr.sin_port=htons(8000); //Server port number
 
-    /*创建客户端套接字--IPv4协议，面向无连接通信，UDP协议*/
+    /*Create a client socket-IPv4 protocol for connectionless communication, UDP protocol*/
 	if((client_sockfd=socket(PF_INET,SOCK_DGRAM,0))<0)
 	{  
 		perror("socket error");
 		return 1;
 	}
-	strcpy(buf, bufdata); // 发送的内容
+	strcpy(buf, bufdata); // The content sent
 	printf("sending: '%s'/n",buf); 
 	sin_size=sizeof(struct sockaddr_in);
 	
-	/*向服务器发送数据包*/
+	/*Send packets to the server*/
 	if((len=sendto(client_sockfd,buf,strlen(buf),0,(struct sockaddr *)&remote_addr,sizeof(struct sockaddr)))<0)
 	{
 		perror("recvfrom"); 
 		return 1;
 	}
 
-	/*关闭套接字*/
+	/*close socket*/
 	close(client_sockfd);
 
 	return 0;
--- midware/src/midware.cpp
+++ midware/src/midware.cpp
@@ -18,23 +18,23 @@ int main(int argc, char *argv[])
 	int server_sockfd;
 	int len;
 	
-	struct sockaddr_in my_addr;   //服务器网络地址结构体
-    struct sockaddr_in remote_addr; //客户端网络地址结构体
+	struct sockaddr_in my_addr;   //Server network address structure
+    struct sockaddr_in remote_addr; //Client network address structure
 	socklen_t sin_size;
-	char buf[BUFSIZ];  //数据传送的缓冲区
-	memset(&my_addr,0,sizeof(my_addr)); //数据初始化--清零
-	my_addr.sin_family=AF_INET; //设置为IP通信
-	my_addr.sin_addr.s_addr=INADDR_ANY;//服务器IP地址--允许连接到所有本地地址上
-	my_addr.sin_port=htons(9999); //服务器端口号
+	char buf[BUFSIZ];  //define a buff
+	memset(&my_addr,0,sizeof(my_addr)); //Data initialization-set zero
+	my_addr.sin_family=AF_INET; //Set to IP communication
+	my_addr.sin_addr.s_addr=INADDR_ANY;//Server IP address-allow connection to all local addresses
+	my_addr.sin_port=htons(9999); //Server port number
 	
-	/*创建服务器端套接字--IPv4协议，面向无连接通信，UDP协议*/
+	/*Create server-side socket-IPv4 protocol, for connectionless communication, UDP protocol*/
 	if((server_sockfd=socket(PF_INET,SOCK_DGRAM,0))<0)
 	{  
 		perror("socket error");
 		return 1;
 	}
 	
-	/*将套接字绑定到服务器的网络地址上*/
+	/*Bind the socket to the server's network address*/
 	if (bind(server_sockfd,(struct sockaddr *)&my_addr,sizeof(struct sockaddr))<0)
 	{
 		perror("bind error");
@@ -43,7 +43,7 @@ int main(int argc, char *argv[])
 	sin_size=sizeof(struct sockaddr_in);
 	printf("waiting for a packet.../n");
 	
-	/*接收客户端的数据并将其发送给客户端--recvfrom是无连接的*/
+	/*Receive data from the client and send it to the client - recvfrom is connectionless*/
 	if((len=recvfrom(server_sockfd,buf,BUFSIZ,0,(struct sockaddr *)&remote_addr,&sin_size))<0)
 	{
 		perror("recvfrom error"); 
@@ -53,25 +53,24 @@ int main(int argc, char *argv[])
 	printf("received packet from %s:/n",inet_ntoa(remote_addr.sin_addr));
 	buf[len]='/0';
 
-	//读取json转化字符串,将json内容赋值给变量
+	//read json and transform to string, Assign json content to variable
 	Json::Reader reader;  
     Json::Value value; 
     reader.parse(buf, value);
     std::string gettype = value["keyword"].asString();  
     std::cout << gettype << std::endl;
 
-	/* 例子
+	/* example:
 	result["score"] = 0.8;
     result["root"] = "商品-容器";
     result["keyword"] = "瓶子";*/
     
-	//商品类型<->数字类型 info定义一个字典
-	/*
-		0:可回收
-		1:有害
-		2:厨余
-		3:其他
-		4无效数据
+	/*Product Types<->number define info as a dictionary
+		0:recyclable trash
+		1:Hazardous waste
+		2:Kitchen waste 
+		3:other garbage
+		4:Invalid data
 	*/
 	Json::Value typeinfo;
 	typeinfo["瓶子"] = 0;
@@ -80,27 +79,27 @@ int main(int argc, char *argv[])
 	typeinfo["药品"] = 1;
 	typeinfo["电池"] = 1;
 	
-	int typenum = 4;//初始化typenum
+	int typenum = 4;//init typenum as a invalid data
 	if(typeinfo[gettype].isString())
 	{
 		std::string typenumber = typeinfo[gettype].asString();
 		typenum = std::stoi(typenumber);
 	}
 	else 
-		typenum = 3;//未找到数据 返回为其他垃圾
+		typenum = 3;//can't find the info in dict. return as other garbage
 
-	//调用周姐函数操作舵机
+	/*Call the function to operate the rudder*/
 	if(typenum < 4 && typenum > -1)
 	{
-		servo::servo s1 = servo();
+		servo s1 = servo();
 		s1.set_servo_open(typenum);
 		s1.set_servo_close(typenum);
 	}
 
-	//TCP传给服务器 o
+	/*send info to the server*/
 	sendtomyserver(buf);
 
-	/*关闭套接字*/
+	/*close socket*/
 	close(server_sockfd);
 
     return 0;
